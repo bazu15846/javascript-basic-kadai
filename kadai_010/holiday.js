6d506fc1d8347c6667f11a498a3c8d94b331335c
bazu15846 <akmagtk@gmail.com>
2024-12-19 20:40:06 +0900

    Add files via upload

--- kadai_010/holiday.js
+++ kadai_010/holiday.js
@@ -2,7 +2,7 @@ const holidays = ["正月","成人の日","建国記念日","天皇誕生日","�
 console.log(holidays);
 
 // for文の場合
-for (let i =0;i<=16;i++) {
+for (let i =0;i<=15;i++) {
     console.log(holidays[i]);
 }
 // while文の場合
